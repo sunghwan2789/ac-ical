564b9cd560fff7406b44c41b2a032eba9473d467
Sunghwan Bang <sunghwan2789@gmail.com>
2019-12-02 02:30:57 +0900

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Ical.Net;
+using Ical.Net.Serialization;
+
+namespace sch_academic_calendar
+{
+    class Program
+    {
+        static readonly HtmlWeb client = new HtmlWeb();
+
+        static async IAsyncEnumerable<AcademicEvent> GetAcademicSchedules()
+        {
+            var seedUrl = @"https://homepage.sch.ac.kr/sch/05/05010001.jsp?mode=list&board_no=20110224223754285127";
+            var baseUri = new Uri(seedUrl);
+            do
+            {
+                Console.WriteLine(seedUrl);
+                var doc = client.Load(seedUrl);
+
+                foreach (var scheduleAnchor in doc.DocumentNode.SelectNodes("//table//a"))
+                {
+                    var scheduleUri = new Uri(baseUri, scheduleAnchor.Attributes["href"].DeEntitizeValue);
+
+                    var sdoc = client.Load(scheduleUri);
+                    var contents = sdoc.DocumentNode.SelectNodes("//table//td");
+                    yield return new AcademicEvent
+                    {
+                        Url = scheduleUri.ToString(),
+                        Title = contents.ElementAt(0).InnerText,
+                        Begin = DateTime.Parse(contents.ElementAt(1).InnerText),
+                        Content = WebUtility.HtmlDecode(contents.ElementAt(2).InnerText),
+                    };
+
+                    await Task.Delay(300);
+                }
+
+                var next = doc.DocumentNode.SelectSingleNode("//div[contains(@class, 'box_pager')]/span/following-sibling::a");
+                seedUrl = next != null ? new Uri(baseUri, next.Attributes["href"].DeEntitizeValue).ToString() : null;
+            }
+            while (!string.IsNullOrEmpty(seedUrl));
+        }
+
+        static async Task Main(string[] args)
+        {
+            var calendar = new Calendar();
+            var dest = args.FirstOrDefault();
+
+            await foreach (var schedule in GetAcademicSchedules())
+            {
+                calendar.Events.Add(schedule.ToCalendarEvent());
+            }
+
+            var serializer = new CalendarSerializer(calendar);
+            if (dest != null)
+            {
+                using var fs = File.OpenWrite(dest);
+                using var sw = new StreamWriter(fs);
+                sw.Write(serializer.SerializeToString());
+            }
+            else
+            {
+                Console.WriteLine(serializer.SerializeToString());
+            }
+        }
+    }
+}
